d5be2bf174804067e886932e0778f81d093a84b5
Golnaz <93345507+Golnaz-spa@users.noreply.github.com>
2023-09-21 14:40:56 -0500

    Add files via upload
    This web application offers data for S&P 500 stocks, enabling users to visualize volume and closing prices, access historical data, and categorize and display all the sectors within the S&P 500 companies while also providing corresponding visual representations.

---
+++ sp500app.py
@@ -0,0 +1,127 @@
+import streamlit as st
+import pandas as pd
+import base64
+import matplotlib.pyplot as plt
+import yfinance as yf
+
+st.title('S&P 500 App')
+
+
+st.write("""
+
+Shown two plots: **closing** price and **volume** of **S&P 500** stock!
+
+""")
+
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+#define the ticker symbol
+#S&P 500 (^GSPC)
+tickerSymbol = '^GSPC'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2023-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+tickerDf = tickerDf.drop(['Dividends', 'Stock Splits'], axis=1)
+st.write("""
+## Closing price
+""")
+st.line_chart(tickerDf.Close)
+st.markdown("""
+X-axis Label: Date, Y-axis Label: Close
+""")
+
+st.write("""
+## Volume price
+""")
+st.line_chart(tickerDf.Volume)
+st.markdown("""
+X-axis Label: Date, Y-axis Label: Volume
+""")
+
+
+
+st.subheader('  ')
+# Display the first 3 rows of tickerDf as a table
+st.subheader("""
+S&P 500 Historical data
+shown 5 rows of the S&P 500 stock
+""")
+st.write(tickerDf.tail(5))  # Display the first 3 rows
+
+st.subheader('  ')
+st.markdown("""
+## S&P 500 list of companies group by each sector
+This app retrieves the list of the **S&P 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)!
+* **Python libraries:** base64, pandas, streamlit, matplotlib
+* **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies).
+""")
+
+st.sidebar.header('User Input Features')
+
+# Web scraping of S&P 500 data
+#
+@st.cache_data
+def load_data():
+    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+    html = pd.read_html(url, header = 0)
+    df = html[0]
+    return df
+
+df = load_data()
+sector = df.groupby('GICS Sector')
+
+# Sidebar - Sector selection
+sorted_sector_unique = sorted( df['GICS Sector'].unique() )
+selected_sector = st.sidebar.multiselect('Sector', sorted_sector_unique, sorted_sector_unique)
+
+# Filtering data
+df_selected_sector = df[ (df['GICS Sector'].isin(selected_sector)) ]
+
+st.header('Display Companies in Selected Sector')
+st.write('Data Dimension: ' + str(df_selected_sector.shape[0]) + ' rows and ' + str(df_selected_sector.shape[1]) + ' columns.')
+st.dataframe(df_selected_sector)
+
+# Download S&P500 data
+# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="SP500.csv">Download CSV File</a>'
+    return href
+
+st.markdown(filedownload(df_selected_sector), unsafe_allow_html=True)
+
+# https://pypi.org/project/yfinance/
+
+data = yf.download(
+        tickers = list(df_selected_sector[:10].Symbol),
+        period = "ytd",
+        interval = "1d",
+        group_by = 'ticker',
+        auto_adjust = True,
+        prepost = True,
+        threads = True,
+        proxy = None
+    )
+
+# Plot Closing Price of Query Symbol
+def price_plot(symbol):
+  df = pd.DataFrame(data[symbol].Close)
+  df['Date'] = df.index
+  fig, ax = plt.subplots()
+  ax.fill_between(df.Date, df.Close, color='skyblue', alpha=0.3)
+  ax.plot(df.Date, df.Close, color='skyblue', alpha=0.8)
+  plt.xticks(rotation=90)
+  plt.title(symbol, fontweight='bold')
+  plt.xlabel('Date', fontweight='bold')
+  plt.ylabel('Closing Price', fontweight='bold')
+  return st.pyplot(fig)
+
+num_company = st.sidebar.slider('Number of Companies', 1, 5)
+
+if st.button('Show Plots'):
+    st.header('Stock Closing Price')
+    for i in list(df_selected_sector.Symbol)[:num_company]:
+        price_plot(i)
